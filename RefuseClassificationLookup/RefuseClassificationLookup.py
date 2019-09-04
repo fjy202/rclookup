0fa678cb8a3ad06a33c00d4ec1e0ea6068100c5c
Your Name <you@example.com>
2019-09-04 11:25:23 +0800

    rclookup done.

--- RefuseClassificationLookup/RefuseClassificationLookup.py
+++ RefuseClassificationLookup/RefuseClassificationLookup.py
@@ -1,1 +1,21 @@
+import requests
+from sys import exit
+import json
 
+#settings
+base="https://sffc.sh-service.com/wx_miniprogram/sites/feiguan/trashTypes_2/Handler/Handler.ashx?a=GET_KEYWORDS&kw=%s"
+
+def getResultDict(kw):
+    json_=requests.get(base%kw).text
+    return json.loads(json_)
+def main():
+    while 1:
+        x=input("> ")
+        if x=="":
+            continue
+        result=getResultDict(x) if x!="exit" else exit()
+        if result["kw_arr"]!=None:
+            for x in result["kw_arr"]:
+                print(x["Name"]+":"+x["TypeKey"]+"\n"+x["Note"])
+if __name__=="__main__":
+    main()
